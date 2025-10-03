c07b303fc5eb8f2d0f22c86e59e076ddd243a9f8
OKABE <sumust2@gmail.com>
2025-10-04 01:20:14 +0900

    クラスの役割の確認とConfigクラスを追加

--- src/main/java/com/example/demo/Menter01Application.java
+++ src/main/java/com/example/demo/Menter01Application.java
@@ -2,10 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * メンターアプリケーションのメインクラス
+ */
 @SpringBootApplication
 public class Menter01Application {
-
+//メンターアプリケーションのメインメソッド
 	public static void main(String[] args) {
 		SpringApplication.run(Menter01Application.class, args);
 	}
