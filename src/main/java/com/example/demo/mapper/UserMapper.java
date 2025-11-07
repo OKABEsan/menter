32b68e5549982b9b56410e03e9cbb634ae157305
OKABE <sumust2@gmail.com>
2025-11-08 00:52:47 +0900

    コメント追加

--- src/main/java/com/example/demo/mapper/UserMapper.java
+++ src/main/java/com/example/demo/mapper/UserMapper.java
@@ -7,7 +7,7 @@ import org.apache.ibatis.annotations.Mapper;
 import com.example.demo.model.User;
 import com.example.demo.model.UserSearchForm;
 /**
- * 
+ * SpringBootとSQLを繋ぐ入口になるクラス
  */
 @Mapper
 public interface UserMapper {
