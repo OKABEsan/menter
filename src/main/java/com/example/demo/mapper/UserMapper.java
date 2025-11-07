2dadb44cbc35ea18fc5f07f67dbc7aa56599196a
OKABE <sumust2@gmail.com>
2025-11-08 00:28:58 +0900

    pom.xmlにmy batisを保存し、Mapperクラスのエラーを解消。usersearchformクラスを実装

--- src/main/java/com/example/demo/mapper/UserMapper.java
+++ src/main/java/com/example/demo/mapper/UserMapper.java
@@ -1,7 +1,19 @@
 package com.example.demo.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.example.demo.model.User;
+import com.example.demo.model.UserSearchForm;
+/**
+ * 
+ */
 @Mapper
 public interface UserMapper {
+	
+	
+	List<User>searchUsers(UserSearchForm form);
+
 
 }
---
+++ src/main/java/com/example/demo/model/UserSearchForm.java
@@ -0,0 +1,33 @@
+package com.example.demo.model;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Pattern;
+
+import lombok.Data;
+
+/**
+ *ユーザーの検索フォームから受けたデータを表すクラス
+ */
+@Data
+public class UserSearchForm {
+
+	private Integer id;
+
+	private String username; // ユーザー名を保存するための場所
+
+	@Min(value = 0, message = "年齢は0歳以上で入力してください")
+	private Integer age;
+
+	@Pattern(regexp = "^\\d{4}/\\d{2}/\\d{2}$", message = "生年月日は0000/00/00の型式で入力してください")
+	private String birthday;
+
+	private String email; // メールアドレスを保存するための場所
+
+	@Pattern(regexp = "^0\\d{1,4}-\\d{1,4}-\\d{3,4}$", message = "電話番号の型式が正しくありません")
+	private String tel;
+
+	private String plan;
+
+	private String loginRole;
+
+}
