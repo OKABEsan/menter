34fa8398a3c25da45b3b3396f9dc1ccc69531ba4
OKABE <sumust2@gmail.com>
2025-11-20 01:08:32 +0900

    バグ修正

--- src/main/resources/static/deletemodal.js
+++ src/main/resources/static/deletemodal.js
@@ -2,11 +2,13 @@
 const modalCancelBtn=document.querySelector(".modal-cancel");
 const deleteBtns=document.querySelectorAll(".delete-btn");
 const modalOverlay=document.querySelector(".modal-overlay");
+const deleteUserId=document.getElementById("deleteUserId");
 
 deleteBtns.forEach((btn)=>{
 	
 	btn.addEventListener("click",()=>{
-	
+	const id=btn.getAttribute("delete-id");
+	deleteUserId.value=id;
 	modalOverlay.classList.add("present");
 	
   });
